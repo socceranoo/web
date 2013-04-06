da686532552223f2fd5cfe216a56a2ca8b475e04
socceranoo <socceranoo@gmail.com>
2013-04-05 19:41:45 -0700

    2nd checkin after  new ubuntu
    checkin a minor bug

--- webv2/insertdivs.php
+++ webv2/insertdivs.php
@@ -3,8 +3,8 @@ $count = 7;
 $arr = array("Webmaster's Portfolio", "Resume Builder", "Money Matters", "Game Room", "My Music", "SyncTube" , "My Recipies");
 $linkarr = array("portfolio", "../RegForm/resume-home.php", "../RegForm/money-matters.php", "trump/", "audio.php", "../RegForm/synctube-home.php" , "../RegForm/recipes.php");
 $imgarr = array("portfolio.jpg", "ResumeBuildTN1.jpg", "money.jpg", "gameroom1TN.jpg", "ftpTN.jpg", "synctubeTN.jpg" , "kitchenTN.jpg");
-$target="";
 for ($i=0; $i < $count; $i++) { 
+	$target="";
 	if ($i == 0){
 		$target="target=\"_blank\"";
 	}
